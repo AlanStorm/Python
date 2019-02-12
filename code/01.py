1416a9e15e4b9681b2e29b23142e1ef2274ecb2d
826041522@qq.com <826sunhao>
2019-02-12 17:54:11 +0800

    Python

--- code/01.py
+++ code/01.py
@@ -671,6 +671,214 @@
 # print(dir(a))
 #
 #
+#
+#
+# # 属性案例
+# # 创建Student类，描述学生类
+# # 学生具有Student.name属性
+# # 但name格式并不统一
+# class Student:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
+#
+#         # 如果不想修改代码
+#         self.set_name(name)
+#
+#     # 介绍下自己
+#     def intro(self):
+#         print("Hai, my name is {0}".format(self.name))
+#
+#     def set_name(self, name):
+#         self.name = name.upper()
+#
+#
+# s1 = Student("LIU Ying", 19)
+# s2 = Student("michi stangle", 24)
+# s1.intro()
+# s2.intro()
+#
+#
+#
+#
+# # property案例
+# # 定义一个Person类，具有name，age属性
+# # 对于任意输入的姓名，我们都希望用大写方式保存
+# # 年龄，我们希望内部统一用整数保存
+# # x = property(fget, fset, fdel, doc)
+# class Person:
+#     """
+#     只是一个人，一个高尚的人，
+#     属相啊
+#     """
+#     # 函数的名称可以随意
+#     def f_get(self):
+#         return self._name * 2
+#
+#     def f_set(self, name):
+#         # 所有输入的姓名以大写形式保存
+#         self._name = name.upper()
+#
+#     def f_del(self):
+#         self._name = "NoName"
+#
+#     name = property(f_get, f_set, f_del, "对name进行下造作")
+#
+#
+# p = Person()
+# p.name = "TuLing"
+# print(p.name)
+#
+#
+# # 类的内置函数举例
+# print(Person.__dict__)
+# print(Person.__doc__)
+# print(Person.__name__)
+# print(Person.__bases__)
+#
+#
+#
+#
+# # init 举例
+# class A:
+#     def __init__(self, name = 0):
+#         print('哈哈，调用了')
+#
+#
+# a = A()
+# print(type(a))
+#
+#
+#
+#
+# # __call__举例
+# class A:
+#     def __init__(self, name = 0):
+#         print('哈哈，调用了')
+#
+#     def __call__(self, *args, **kwargs):
+#         print(args)
+#         print(kwargs)
+#
+#
+# a = A()
+# a()
+#
+#
+#
+#
+# # __call__举例
+# class A:
+#     def __init__(self, name = 0):
+#         print('哈哈，调用了')
+#
+#     def __call__(self, *args, **kwargs):
+#         print(args)
+#         print(kwargs)
+#
+#     def __str__(self):
+#         return '例子'
+#
+#
+# a = A()
+# print(a)
+#
+#
+#
+#
+# # __getattr__
+# class A:
+#     name = "NoName"
+#     age = 18
+#
+#     def __getattr__(self, item):
+#         return "NO"
+#
+#
+# a = A()
+# print(a.name)
+# print(a.address)
+#
+#
+#
+#
+# # __setattr__ 案例
+# class Person:
+#     def __init__(self):
+#         pass
+#
+#     def __setattr__(self, key, value):
+#         print("设置属性：{0}".format(key))
+#         # 下面语句会导致问题，死循环
+#         # self.name = value
+#         # 此种情况，为了避免死循环，规定统一嗲用父类魔法函数
+#         super(Person, self).__setattr__(key, value)
+#
+#
+# p = Person()
+# print(p.__dict__)
+# p.age = 18
+# print(p.__dict__)
+#
+#
+#
+#
+# # __gt__
+# class Student:
+#     def __init__(self, name):
+#         self._name = name
+#
+#     def __gt__(self, other):
+#         print("哈哈，{0} 会比 {1} 大吗".format(self, other))
+#         return self._name > other._name
+#
+#     def __str__(self):
+#         return self._name
+#
+#
+# stu1 = Student('onw')
+# stu2 = Student('two')
+# print(stu1 > stu2)
+#
+#
+#
+#
+# # 三种方法的案例
+# class Person:
+#     # 实例方法
+#     def eat(self):
+#         print(self)
+#         print("Eating...")
+#
+#     # 类方法
+#     # 类方法的第一个参数，一般命名为cls，区别于self
+#     @classmethod
+#     def play(cls):
+#         print(cls)
+#         print("Playing...")
+#
+#     # 静态方法
+#     # 不需要用第一个参数表示自身或者类
+#     @staticmethod
+#     def say():
+#         print("Saying...")
+#
+#
+# yueyue = Person()
+#
+# # 实例方法
+# yueyue.eat()
+# # 类方法
+# Person.play()
+# yueyue.play()
+# # 静态方法
+# Person.say()
+# yueyue.say()
+#
+#
+
+
+
 
 
 
