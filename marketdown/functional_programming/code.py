83a5d31215f4958b38824de5d571acea3fff3ac0
826041522@qq.com <826sunhao>
2019-04-04 19:45:02 +0800

    Python

--- marketdown/functional_programming/code.py
+++ marketdown/functional_programming/code.py
@@ -67,3 +67,43 @@
 #
 # # 需求变更，需要把n放大三十倍，此时funB则无法实现
 # print(funC(7, funD))
+# # map举例
+# # 有一个列表，想对列表李的每一个元素乘以10，并得到新的礼列表
+# l1 = [i for i in range(10)]
+#
+#
+# # print(l1)
+# # l2 = []
+# # for i in l1:
+# #     l2.append(i * 10)
+#
+#
+# # print(l2)
+#
+#
+# # 利用map实现
+# def mulTen(n):
+#     return n * 10
+#
+#
+# l3 = map(mulTen, l1)
+# # map类型是一个可迭代的结构，所以可以使用for遍历
+# for i in l3:
+#     print(i)
+# # 以下列表生成式得到的结果为空，why？
+# l4 = [i for i in l3]
+# print(l4)
+#
+#
+from functools import reduce
+
+
+# 定义一个操作函数
+# 加入操作函数只是想家
+def myAdd(x, y):
+    return x + y
+
+
+# 对于列表[1,2,3,4,5,6]执行myAdd的reduce操作
+rst = reduce(myAdd, [1, 2, 3, 4, 5, 6])
+print(rst)
