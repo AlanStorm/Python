1036ab6a14a4bb4b54921c20499954bff53ff71b
826041522@qq.com <826sunhao>
2019-09-11 17:20:56 +0800

    practice

--- marketdown/Django/django_first/polls/urls.py
+++ marketdown/Django/django_first/polls/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('index_1', views.index_1, name='index_1')
+    path('<int:year>/', views.index, name='index'),
+    path('index_1/', views.index_1, name='index_1'),
+    re_path(r'index_2/(?:page-(?P<page_number>\d+)/)?$', views.index_2, name='index_2'),
 ]
--- marketdown/Django/django_first/polls/views.py
+++ marketdown/Django/django_first/polls/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.handlers.wsgi import WSGIRequest
 import json
 
 
 # Create your views here.
 
-def index(request):
+def index(request, year):
+    test(request)
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
 def index_1(request):
-    print(type(request))
-    print(request.__dict__)
     return HttpResponse('test')
+
+
+def index_2(request, page_number):
+    print(page_number)
+    return HttpResponse('page number is {0}'.format(page_number))
+
+
+def test(request):
+    # print(type(request))  # 打印出request的类型
+    print(request.__dict__)  # 打印出request的header详细信息
+    # 循环打印出每一个键值对
+    # for k, v in request.environ.items():
+    # print(k, v)
