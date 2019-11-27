0a3b7ce1b88f61c1d80fd010a0e95acd27f9e830
826041522@qq.com <826sunhao>
2019-11-27 10:39:24 +0800

    practice

--- algorithm/1.py
+++ algorithm/1.py
@@ -16,5 +16,55 @@ def binary_search(list, item):
 
 my_list = [1, 3, 5, 7, 9]
 
-print(binary_search(my_list, 3))
-print(binary_search(my_list, -1))
+
+# print(binary_search(my_list, 3))
+# print(binary_search(my_list, -1))
+
+
+def find_smallest(arr):
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+
+def selection_sort(arr):
+    new_arr = []
+    for i in range(len(arr)):
+        smallest = find_smallest(arr)
+        new_arr.append(arr.pop(smallest))
+    return new_arr
+
+
+# print(selection_sort([5, 3, 6, 2, 10]))
+
+
+def greet(name):
+    print("hello, " + name + "!")
+    greet2(name)
+    print("getting ready to say bye...")
+    bye()
+
+
+def greet2(name):
+    print("how are you," + name + "?")
+
+
+def bye():
+    print('ok bye')
+
+
+# greet('maggie')
+
+def sum(arr):
+    total = 0
+    for x in arr:
+        total += x
+    return total
+
+
+# print(sum([1, 2, 3, 4]))
+
